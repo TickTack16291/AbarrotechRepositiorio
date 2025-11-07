c7529bc4ad62562a06f623c9d54b8752966d7364
Brayan Rios <bjrf81440@gmail.com>
2025-11-06 22:05:00 -0500

    Cambios en ventas

--- LoginDeAbarrotech/Ventas.xaml.cs
+++ LoginDeAbarrotech/Ventas.xaml.cs
@@ -68,13 +68,15 @@ namespace LoginDeAbarrotech
         {
             // Da un error que no entiendo, lastima es un buen detalle jaja
         }
-        
+        /// <summary>
+        /// La clase "ProductoSeleccionado se usa para agregarle un campo de cantidad a los productos, se hace herencia"
+        /// </summary>
         internal class ProductoSeleccionado : Producto
         {
             public int cantidad { get; set; } = 0;
         }
-        float total = 0.0f;
-        List<ProductoSeleccionado> productosSeleccionados = new List<ProductoSeleccionado>();
+        float total = 0.0f;// Total de la compra
+        List<ProductoSeleccionado> productosSeleccionados = new List<ProductoSeleccionado>();// Esta aqui para que no se reinicie cada que se llame el evento
         private void dg_ProductosDisponibles_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var prseleccionado = dg_ProductosDisponibles.SelectedItem as Producto;
@@ -111,6 +113,14 @@ namespace LoginDeAbarrotech
             Txt_TotalVenta.Text = total.ToString();
             dg_ProductosSeleecionados.Items.Refresh();// Es para forzar que se actualize la tabla
             dg_ProductosDisponibles.SelectedItem = null;
+        }
+        private void btn_RealizarVenta_Click(object sender, RoutedEventArgs e)
+        {
+            ConexionBD conexion = new ConexionBD();
+            long idAux = conexion.ObtenerIdUsuario(LoginAbarrotech.UsuarioGlobal);
+            conexion.RealizarVenta(idAux, DateTime.Now, total, "Efectivo", 1);
+            // Habria que hacer una ventana en donde selecciones el metodo de pago ademas de que te deberia dar un resumen de la venta
+            // Total - Cant. de productos - Usuario que realiza la venta - hora y fecha - numero de caja
         }
     }
 }
